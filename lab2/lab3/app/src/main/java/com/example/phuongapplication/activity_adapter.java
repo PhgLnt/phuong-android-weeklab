e7a9af092adb628b42075eba3ce3136894d26c0a
RouiePLNT <18521272@gm.uit.edu.vn>
2022-04-26 10:03:15 +0700

    Create activity_adapter.java

---
+++ lab2/lab3/app/src/main/java/com/example/phuongapplication/activity_adapter.java
@@ -0,0 +1,14 @@
+package com.example.phuongapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_adapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adapter);
+    }
+}
